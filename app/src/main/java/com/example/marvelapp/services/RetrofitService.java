61244a5a27bfbd04d63f13f71e8b37ce8c74f5cf
Eduardo Freire <61304055+EduardoGhost@users.noreply.github.com>
2021-10-24 23:42:12 -0300

    bora1

--- app/src/main/java/com/example/marvelapp/services/RetrofitService.java
+++ app/src/main/java/com/example/marvelapp/services/RetrofitService.java
@@ -1,36 +1,43 @@
 package com.example.marvelapp.services;
 
+import dagger.Module;
+import dagger.Provides;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+
+import javax.inject.Singleton;
+
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
-
-
+import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
+
+
+//@Module: Anotação utilizada em classes que fornecem dependências.
+//@Provides: Anotação utilizadas em métodos que proveem dependências, os métodos provedores são encontrados nas classes com a anotação @Module.
+
+@Module
 public class RetrofitService {
 
     private static final String BASE_URL = "http://gateway.marvel.com/v1/public/";
 
-    private Retrofit retrofit;
+    //talves mudar para estatico
+   @Provides @Singleton
+   Retrofit proverRetrofit(){
+       return new Retrofit.Builder()
+               .baseUrl(BASE_URL)
+               .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+               .addConverterFactory(GsonConverterFactory.create())
+               .build();
 
-    public RetrofitService(Retrofit retrofit) {
-        retrofit = retrofit;
-    }
-
-    GsonBuilder gsonBuilder = new GsonBuilder();
-    Gson gson = gsonBuilder.create();
-
-
-
-        retrofit = new Retrofit.Builder()
-                .baseUrl(BASE_URL)
-                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-                .addConverterFactory(GsonConverterFactory.create(gson))
-                .build();
+   }
 
 
     }
-}
+
+    
+
 
 
